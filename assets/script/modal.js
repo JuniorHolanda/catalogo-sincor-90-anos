dd14f1bb19d46e76731e61bfbef1970065b01c8f
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-04-15 19:49:09 -0300

    botão de fechar modal

--- assets/script/modal.js
+++ assets/script/modal.js
@@ -1,10 +1,17 @@
 const modal = document.querySelector('#modal');
-const btnAbrirModal = document.querySelectorAll('.btn')
+const btnAbrirModal = document.querySelectorAll('.btn');
 console.log(btnAbrirModal);
+const btnFecharModal = document.querySelector('#closeModal');
 
 btnAbrirModal[1].addEventListener('click' , () => {
     modal.classList.add('modal-style')
     modal.showModal();
 });
+
+btnFecharModal.addEventListener('click' , () => {
+    modal.classList.remove('modal-style')
+    modal.close()
+
+})
 
 
