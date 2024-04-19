fad79ed5d7b7d00af5443e279ad010126749764f
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-04-18 21:43:38 -0300

    botões funcioanndo
    cada botão está abrindo o modal, agora preciso que cada botão abra um modal diferente do outro

--- assets/script/modal.js
+++ assets/script/modal.js
@@ -3,16 +3,25 @@ let modal = document.querySelector('#modal');
 let btnAbrirModal = document.querySelectorAll('.btn');
 let btnFecharModal = document.querySelector('#closeModal');
 
+function fecharModal () {
+    modal.close();
+    modal.classList.remove('modal-style')
+}
 
-
-function minhaFuncao (idDoElemento) {
-    console.log(idDoElemento);
+function abrirModal (idDoElemento) {
+    modal.classList.add('modal-style')
+    modal.show();
+    
 }
 
 for (i=0; i < btnAbrirModal.length; i++){
     var botao = btnAbrirModal[i];
     botao.addEventListener('click' , function() {
         var idDoElemento = this.id;
-        minhaFuncao(idDoElemento);
+        abrirModal(idDoElemento);
     });
 }
+
+btnFecharModal.addEventListener('click' , function () {
+    fecharModal();
+})
